a651b9ad26c0baffa413c639263115d7d8dafd3a
AnkitPatro <ankitpatro009@gmail.com>
2020-06-10 18:33:58 -0400

    Adding Hibernate changes

--- src/main/java/com/patro/SpringBootProject/datasource/PostgresDatasource.java
+++ src/main/java/com/patro/SpringBootProject/datasource/PostgresDatasource.java
@@ -1,17 +1,17 @@
-package com.patro.SpringBootProject.datasource;
-
-
-import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class PostgresDatasource {
-    @Bean
-    @ConfigurationProperties("app.datasource")
-    public HikariDataSource hikariDataSource() {
-        return DataSourceBuilder.create().type(HikariDataSource.class).build();
-    }
-}
+//package com.patro.SpringBootProject.datasource;
+//
+//
+//import com.zaxxer.hikari.HikariDataSource;
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+//import org.springframework.boot.jdbc.DataSourceBuilder;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class PostgresDatasource {
+//    @Bean
+//    @ConfigurationProperties("app.datasource")
+//    public HikariDataSource hikariDataSource() {
+//        return DataSourceBuilder.create().type(HikariDataSource.class).build();
+//    }
+//}
