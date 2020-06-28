8ba96b841d9483c43f165186e5e688aa5637383a
AnkitPatro <ankitpatro009@gmail.com>
2020-06-28 12:55:55 -0400

    Changing lifecycle hooks

--- codedeploy/afterInstall.sh
+++ codedeploy/afterInstall.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+
+pwd
+ls
+cd ~
+chmod 755 webapp-0.0.1-SNAPSHOT.jar
+java -DDB_PASSWORD=Ankit#1992 -jar webapp-0.0.1-SNAPSHOT.jar &
--- codedeploy/appstart.sh
+++ codedeploy/appstart.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-pwd
-ls
-cd ~
-chmod 755 webapp-0.0.1-SNAPSHOT.jar
-java -DDB_PASSWORD=Ankit#1992 -jar webapp-0.0.1-SNAPSHOT.jar &
+
