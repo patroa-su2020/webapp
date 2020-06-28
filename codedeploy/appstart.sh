74f44317de5b0590d1f48d2530e4e53d4ca758d3
AnkitPatro <ankitpatro009@gmail.com>
2020-06-28 13:40:32 -0400

    Long running process fix

--- codedeploy/appstart.sh
+++ codedeploy/appstart.sh
@@ -5,4 +5,4 @@ pwd
 ls
 cd ~
 chmod 755 webapp-0.0.1-SNAPSHOT.jar
-java -DDB_PASSWORD=Ankit#1992 -jar webapp-0.0.1-SNAPSHOT.jar &
+java -DDB_PASSWORD=Ankit#1992 -jar webapp-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
