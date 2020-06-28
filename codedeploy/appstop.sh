75741e731bbbef65e8fffa11348580d751699850
AnkitPatro <ankitpatro009@gmail.com>
2020-06-28 04:06:11 -0400

    Removing timeout command after app start

--- codedeploy/appstop.sh
+++ codedeploy/appstop.sh
@@ -1,1 +1,2 @@
 #!/bin/bash
+sudo lsof -i:8080 -t | xargs -r sudo kill
--- codedeploy/beforeInstall.sh
+++ codedeploy/beforeInstall.sh
@@ -6,4 +6,4 @@ pid=$(ps aux | pgrep -f webapp)
 echo "PID To kill"
 echo "$pid"
 
-sudo lsof -i:8080 -t | xargs -r sudo kill
+
