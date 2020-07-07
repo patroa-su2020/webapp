5572b277f4775d70f9a7a0aa0ac003d0826b96b1
AnkitPatro <ankitpatro009@gmail.com>
2020-07-07 17:39:01 -0400

    Adding cloudwatch-config.json path in afterInstall.sh

--- codedeploy/afterInstall.sh
+++ codedeploy/afterInstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/cloudwatch-config.json -s
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/ubuntu/opt/cloudwatch-config.json -s
 
 
