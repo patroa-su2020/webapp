de10b4e55925e9ce2c16e5757f4ce04fcaadf60b
AnkitPatro <ankitpatro009@gmail.com>
2020-07-08 19:32:49 -0400

    giving proper webapp.sh name in after install

--- codedeploy/afterInstall.sh
+++ codedeploy/afterInstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/ubuntu/opt/cloudwatch-config.json -s
-sudo chmod +x /usr/local/bin/webappService.sh
+sudo chmod +x /usr/local/bin/webapp.sh
 #/usr/local/bin/./Service_Name.sh start
 #/usr/local/bin/./Service_Name.sh stop
 #/usr/local/bin/./Service_Name.sh restart
