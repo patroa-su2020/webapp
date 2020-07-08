328b209434829405e124fc2567a933a1190e47b6
AnkitPatro <ankitpatro009@gmail.com>
2020-07-08 19:26:26 -0400

    Adding webapp.sh for the java service

--- codedeploy/afterInstall.sh
+++ codedeploy/afterInstall.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/ubuntu/opt/cloudwatch-config.json -s
+sudo chmod +x /usr/local/bin/webappService.sh
+#/usr/local/bin/./Service_Name.sh start
+#/usr/local/bin/./Service_Name.sh stop
+#/usr/local/bin/./Service_Name.sh restart
 
 
