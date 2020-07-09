5b82b5b1238cf82a36ebc6288de82b34c5ef8639
AnkitPatro <ankitpatro009@gmail.com>
2020-07-09 04:28:23 -0400

    beforeInstall : adding change directory

--- codedeploy/beforeInstall.sh
+++ codedeploy/beforeInstall.sh
@@ -15,6 +15,7 @@ nohup java -DDB_PASSWORD=Ankit#1992 -jar /home/ubuntu/webapp-0.0.1-SNAPSHOT.jar
 START
 chmod 755 /home/ubuntu/applicationScript.sh
 echo "applicationScript created"
+cd /etc/profile.d
 sudo cp /home/ubuntu/applicationScript.sh applicationScript.sh
 sudo chmod 755 /etc/profile.d/applicationScript.sh
 
