934ad6e5f9e4f71c470fed0a0ce3d85cc0f07bd2
AnkitPatro <ankitpatro009@gmail.com>
2020-07-09 04:19:53 -0400

    beforeInstall

--- codedeploy/beforeInstall.sh
+++ codedeploy/beforeInstall.sh
@@ -6,4 +6,18 @@ pid=$(ps aux | pgrep -f webapp)
 echo "PID To kill"
 echo "$pid"
 
+cd /home/ubuntu
+sudo cat > applicationScript.sh << START
+#!/bin/bash
+cd /home/ubuntu
+echo "Inside /home/ubuntu"
+nohup java -DDB_PASSWORD=Ankit#1992 -jar /home/ubuntu/webapp-0.0.1-SNAPSHOT.jar
+START
+chmod 755 /home/ubuntu/applicationScript.sh
+echo "applicationScript created"
+sudo cp /home/ubuntu/applicationScript.sh applicationScript.sh
+sudo chmod 755 /etc/profile.d/applicationScript.sh
+
+
+
 
