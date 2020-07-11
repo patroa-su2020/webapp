9f32d505508a3c116137311d587c68b6e1fbe1ba
AnkitPatro <ankitpatro009@gmail.com>
2020-07-11 18:37:12 -0400

    Adding the startup command in afterInstall

--- codedeploy/afterInstall.sh
+++ codedeploy/afterInstall.sh
@@ -5,5 +5,17 @@ sudo chmod +x /usr/local/bin/webapp.sh
 #/usr/local/bin/./Service_Name.sh start
 #/usr/local/bin/./Service_Name.sh stop
 #/usr/local/bin/./Service_Name.sh restart
+cd /home/ubuntu
+sudo cat > applicationScript.sh << START
+#!/bin/bash
+cd /home/ubuntu
+echo "Inside /home/ubuntu"
+nohup java -DDB_PASSWORD=Ankit#1992 -jar /home/ubuntu/webapp-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
+START
+chmod 755 /home/ubuntu/applicationScript.sh
+echo "applicationScript created"
+cd /etc/profile.d
+sudo cp /home/ubuntu/applicationScript.sh applicationScript.sh
+sudo chmod 755 /etc/profile.d/applicationScript.sh
 
 
